4a5459bb3ce70ed3b6ed3b9fefdd0923c96824b3
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-01-28 10:13:18 +0200

    issue details page

---
+++ src/app/dashboard/issues/details/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import IssueDetails from "./issueDetails";
+
+const Page = () => {
+     return (
+          <div>
+               <IssueDetails />
+          </div>
+     );
+};
+
+export default Page;
